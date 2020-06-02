40af67cb58597b9af37d09eef63b273c59c2dbd3
Virna18 <virna_ridderbos@hotmail.com>
2020-06-02 15:12:36 +0200

    opdracht 1 2e push

--- src/Demo.java
+++ src/Demo.java
@@ -1,14 +1,16 @@
+import java.util.Scanner;
+
 public class Demo {
     public static void main (String [] args){
         Persoon jan = new Persoon();
         Persoon kees = new Persoon();
         Bank bank = new Bank();
-        System.out.println(jan.portemonnee.euros);
-        bank.geldopnemen(jan);
-        bank.geldopnemen(jan);
-        bank.geldopnemen(jan);
-        bank.geldopnemen(jan);
-        System.out.println(jan.portemonnee.euros);
+        System.out.println("Jan heeft " + jan.portemonnee.euros + " euro in zijn portemonnee");
+        bank.geldOpnemen(jan);
+        System.out.println("Jan heeft na het opnemen van geld " + jan.portemonnee.euros + "euro in zijn portemonnee");
+        System.out.println(kees.portemonnee.euros);
+        bank.geldStorten(kees);
+        System.out.println(kees.portemonnee.euros);
     }
 
 }
@@ -16,10 +18,22 @@ public class Demo {
 class Bank {
     int balans = 150000;
     int opnamebedrag = 500;
-    void geldopnemen(Persoon persoon){
+    Scanner sc = new Scanner(System.in);
+    void geldOpnemen(Persoon persoon){
+        System.out.println("U heeft gekozen om geld op te nemen, voer uw 4 cijferige pincode in:");
+        String invoer = sc.next();
         persoon.portemonnee.euros = persoon.portemonnee.euros + opnamebedrag;
         System.out.println("in geldopnemen");
         balans = balans - opnamebedrag;
+        System.out.println(balans);
+    }
+
+    void geldStorten(Persoon persoon){
+        int stortbedrag = 300;
+        System.out.println("Voer uw 4 cijferige pincode in:");
+        String invoer = sc.next();
+        persoon.portemonnee.euros = persoon.portemonnee.euros - stortbedrag;
+        balans = balans - stortbedrag;
         System.out.println(balans);
     }
 }
